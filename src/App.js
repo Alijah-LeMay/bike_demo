f19ece443df81f1d7a4abae8e5ec6d4af18666b2
alijah-tectonics <alijahlemay12@gmail.com>
2023-03-12 15:21:12 -0700

    added first screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
+import LandingScreen from './screens/LandingScreen'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <LandingScreen />
+    </div>
+  )
 }
 
 export default App
